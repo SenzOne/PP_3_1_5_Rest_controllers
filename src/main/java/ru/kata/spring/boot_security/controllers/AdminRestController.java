1041a4e3ef48db4c789a11cbdeed4f370aca8b36
Senz One <sem63sam@yandex.ru>
2024-03-06 18:06:41 +0400

    the task is completed

--- src/main/java/ru/kata/spring/boot_security/controllers/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/controllers/AdminRestController.java
@@ -1,8 +1,6 @@
 package ru.kata.spring.boot_security.controllers;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
